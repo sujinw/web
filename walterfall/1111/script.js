6d8a4e1309d8397b606b358728b86c1e8ac53f6b
sujinw <sujinw@qq.com>
2015-12-02 16:28:41 +0800

    新的改变

---
+++ walterfall/1111/script.js
@@ -0,0 +1,3 @@
+window.addEventListener('load', function(e) {
+  document.querySelector('#test').innerHTML = '1111';
+}, false);
---
+++ walterfall/js瀑布流/script.js
@@ -0,0 +1,3 @@
+window.addEventListener('load', function(e) {
+  document.querySelector('#test').innerHTML = 'js瀑布流';
+}, false);
